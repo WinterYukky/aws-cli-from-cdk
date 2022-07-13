34c51da4806ee0ce6e2053cf8b906e4283214ede
WinterYukky <49480575+WinterYukky@users.noreply.github.com>
2022-07-13 11:38:17 +0000

    Initial commit

---
+++ test/aws-cli-from-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCliFromCdk from '../lib/aws-cli-from-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cli-from-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCliFromCdk.AwsCliFromCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
